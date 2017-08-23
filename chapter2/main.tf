027302113b61df8e5814bff6e68861d709f0027e
anthonyjgatti <anthony.j.gatti@github.com>
2017-08-22 21:19:13 -0400

    Adding auto scaling group.

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -7,7 +7,9 @@ variable "server_port" {
   default = 8080
 }
 
-resource "aws_instance" "example" {
+data "aws_availability_zones" "all" {}
+
+resource "aws_launch_configuration" "example" {
   ami = "ami-44bf9f21"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
@@ -17,6 +19,10 @@ resource "aws_instance" "example" {
                  echo "Hello, World" > index.html
                  nohup busybox httpd -f -p "${var.server_port}" &
                  EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags {
     Name = "terraform-example"
@@ -32,8 +38,28 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propogate_at_launch = true
+  }
 }
 
 output "public_ip" {
   value = "${aws_instance.example.public_ip}"
 }
+
